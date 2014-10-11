f64d643fac6593eebb6845182a9f5d1a89882958
abataloff <abataloff@ya.ru>
2014-10-11 20:09:42 +0400

    Start project

---
+++ abacloud.assistant/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace abacloud.assistant
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //generateRandomFile("random.out");
+            var _sender = new Sender();
+            //_sender.SendFile*("c:\\temp\\#02 Allen.mkv");
+            //_sender.SendFile("c:\\temp\\Code_orig.rar");
+            //_sender.SendFile("random.out");
+            _sender.SendDirectory(@"c:\Users\abatalov\Code\AbaBackup\");
+
+            //Application.Run(new MainWindow());
+        }
+
+        private static void generateRandomFile(string a_fileName)
+        {
+            var _file = File.Create(a_fileName);
+            var _rnd = new Random();
+            for (var _i = 0; _i < 1*1024*1024; _i++)
+            {
+                _file.WriteByte((byte) _rnd.Next(byte.MaxValue));
+            }
+            _file.Close();
+        }
+    }
+}
---
+++ abacloud.assistant/Sender.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Abacloud.ApiClient;
+
+namespace abacloud.assistant
+{
+    class Sender
+    {
+        private readonly Client client = Client.Create("http://127.0.0.1:5001/", "user", "pass");
+
+        public string SendFile(string a_fileName)
+        {
+            string _retVal;
+
+            var _stream = File.Open(a_fileName, FileMode.Open, FileAccess.Read);
+            string _errorMessage;
+            _retVal = client.SendContent(_stream, out _errorMessage);
+            if (string.IsNullOrEmpty(_retVal))
+            {
+                throw new Exception(_errorMessage);
+            }
+            _stream.Close();
+
+            return _retVal;
+        }
+
+        public Dictionary<string,string> SendDirectory(string a_path)
+        {
+            if (!Directory.Exists(a_path))
+                throw new DirectoryNotFoundException(string.Format("Не найден каталог для отправки {0}", a_path));
+            
+            var _retDic = new Dictionary<string, string>();
+
+            var _directoryInfo = new DirectoryInfo(a_path);
+            foreach (var _fileInfo in _directoryInfo.GetFiles())
+            {
+                var _fileName = _fileInfo.FullName;
+                var _guid = SendFile(_fileName);
+                _retDic.Add(_fileName,_guid);
+            }
+            foreach (var _directory in _directoryInfo.GetDirectories())
+            {
+                var _dic = SendDirectory(_directory.FullName);
+                foreach (var _rec in _dic)
+                {
+                    _retDic.Add(_rec.Key, _rec.Value);
+                }
+            }
+
+            return _retDic;
+        }
+    }
+}
